66b2bc684a63ed345a75917cb11502ee0a63efa8
Kishan Kumar Kharwar <KishanK@quinnox.com>
2022-03-28 23:51:21 +0530

    bubble-sort and insertion-sort

---
+++ src/main/java/com/test/TestingBubbleSort.java
@@ -0,0 +1,26 @@
+package com.test;
+
+public class TestingBubbleSort {
+
+  public static void main(String[] args) {
+    int[] arr = {2, 7, 3, 1, 5, 2};
+
+    bubbleSort(arr, arr.length);
+    for(int i : arr){
+      System.out.print(i + " ");
+    }
+
+  }
+
+  private static void bubbleSort(int[] arr, int length) {
+    for (int j = 0; j < length; j++) {
+      for (int i = 0; i < length - 1; i++) {
+        if(arr[i]>arr[i+1]){
+          int temp = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = temp;
+        }
+      }
+    }
+  }
+}
