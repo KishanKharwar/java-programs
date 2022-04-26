888723f6a5b2e4c58ffaba405d42e9752bf49792
Kishan Kumar Kharwar <KishanK@quinnox.com>
2022-04-26 12:40:12 +0530

    tower of hanoi and leetcode adjacent duplicate in string problems solution

---
+++ src/main/java/com/ds/ashokit/recursion/TowerOfHanoi.java
@@ -0,0 +1,23 @@
+package com.ds.ashokit.recursion;
+
+public class TowerOfHanoi {
+
+  public static void main(String[] args) {
+    int noOfDisks = 3;
+    char srcT = 'A';
+    char helpingT = 'B';
+    char destT = 'C';
+    towerOfHanoi(noOfDisks,srcT,destT,helpingT);
+  }
+
+  private static void towerOfHanoi(int noOfDisks, char srcT, char destT, char helpingT) {
+    if(noOfDisks == 0){
+      return;
+    }
+
+    towerOfHanoi(noOfDisks -1, srcT,helpingT,destT);
+    System.out.println("Moved " + noOfDisks + " : [" + srcT+ " -> " +destT+ "]");
+    towerOfHanoi(noOfDisks-1,helpingT,destT,srcT);
+
+  }
+}
---
+++ src/main/java/com/leetcode/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,41 @@
+package com.leetcode;
+
+//https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
+import java.util.Stack;
+
+/**
+ * Input: s = "abbaca" Output: "ca"
+ */
+public class RemoveAllAdjacentDuplicatesInString {
+
+  public static void main(String[] args) {
+    String s = "abbaca";
+    String result = removeDuplicates(s);
+    System.out.println(result);
+  }
+
+  public static String removeDuplicates(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (!stack.isEmpty() && stack.peek() == s.charAt(i)) {
+        stack.pop();
+        continue;
+      }
+      stack.push(s.charAt(i));
+    }
+    if (stack.isEmpty()) {
+      return "";
+    } else {
+      return remove(stack);
+    }
+  }
+
+  public static String remove(Stack st){
+    StringBuilder s = new StringBuilder();
+    while(!st.isEmpty()){
+      s.append(st.pop());
+    }
+    return s.reverse().toString();
+  }
+}
