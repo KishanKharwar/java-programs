c57d6f331a3701ed8f3ff2f942f85946092f5d49
Kishan Kumar Kharwar <KishanK@quinnox.com>
2022-05-18 21:02:39 +0530

    implemented merge sort

--- src/main/java/com/ds/ashokit/custom/linkedlist/MergeSortInLinkedList.java
+++ src/main/java/com/ds/ashokit/custom/linkedlist/MergeSortInLinkedList.java
@@ -4,9 +4,13 @@ package com.ds.ashokit.custom.linkedlist;
 public class MergeSortInLinkedList {
 
   public static void main(String[] args) {
-    int[] arr = {3, 5, 8, -1, 2, 9, 4};
+    int[] arr = {3, 5, 8, -1, 2, 9, 4,1};
+
+
     Node head = createSinglyLinkedList(arr);
     printLinkedList(head);
+//    Node mid = findMidNode(head);
+//    printLinkedList(mid);
 
     Node mergedList = mergeSort(head);
     printLinkedList(mergedList);
@@ -17,26 +21,16 @@ public class MergeSortInLinkedList {
     if (head == null || head.next == null) {
       return head;
     }
-    Node temp = head;
+
     Node mid = findMidNode(head);
+    Node nHead = mid.next;
+    mid.next = null;
 
-    Node firstHalf = mergeSort(head.next);
-    Node secondHalf = mergeSort(mid.next);
-
+    Node firstHalf = mergeSort(head);
+    Node secondHalf = mergeSort(nHead);
     Node result = mergeSortedList(firstHalf, secondHalf);
 
     return result;
-  }
-
-  private static Node findMidNode(Node head) {
-    Node fast = head;
-    Node slow = head;
-
-    while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    return slow;
   }
 
   private static Node mergeSortedList(Node firstHalf, Node secondHalf) {
@@ -45,28 +39,34 @@ public class MergeSortInLinkedList {
 
     while (firstHalf != null && secondHalf != null) {
       if (firstHalf.data > secondHalf.data) {
-        temp.next = secondHalf;
+        head.next = secondHalf;
         secondHalf = secondHalf.next;
       } else {
-        temp.next = firstHalf;
+        head.next = firstHalf;
         firstHalf = firstHalf.next;
       }
-      temp = temp.next;
+      head = head.next;
     }
 
-    while (secondHalf != null) {
-      temp.next = secondHalf;
-      secondHalf = secondHalf.next;
-      temp = temp.next;
+    if(secondHalf != null){
+      head.next = secondHalf;
     }
 
-    while (firstHalf != null) {
-      temp.next = firstHalf;
-      firstHalf = firstHalf.next;
-      temp = temp.next;
+    if(firstHalf != null){
+      head.next = firstHalf;
     }
+    return temp.next;
+  }
 
-    return head.next;
+  private static Node findMidNode(Node head) {
+    Node fast = head;
+    Node slow = head;
+
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
   }
 
   private static Node createSinglyLinkedList(int[] arrOdd) {
@@ -80,7 +80,6 @@ public class MergeSortInLinkedList {
     }
     return head;
   }
-
   private static void printLinkedList(Node head) {
 
     while (head != null) {
