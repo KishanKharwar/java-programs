f89cb1a16e7384163190021201d769a61a83e879
Kishan Kumar Kharwar <KishanK@quinnox.com>
2022-05-27 15:49:12 +0530

    added size and max methods for generic tree

--- src/main/java/com/pepcoding/tree/GenericTree.java
+++ src/main/java/com/pepcoding/tree/GenericTree.java
@@ -8,10 +8,17 @@ public class GenericTree {
   public static void main(String[] args) {
     int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40,
         100, -1, -1, -1};
-    createGenericTree(arr);
+    Node root = createGenericTree(arr);
+    display(root);
+
+    int size = size(root);
+    System.out.println("Size : " + size);
+
+    int max = max(root);
+    System.out.println("Max Node : " + max);
   }
 
-  private static void createGenericTree(int[] arr) {
+  private static Node createGenericTree(int[] arr) {
     Node root = null;
     Stack<Node> stack = new Stack<>();
     for (int i = 0; i < arr.length; i++) {
@@ -30,7 +37,7 @@ public class GenericTree {
       }
 
     }
-    display(root);
+    return root;
   }
 
   private static void display(Node root) {
@@ -47,6 +54,28 @@ public class GenericTree {
     for (Node child : root.children) {
       display(child);
     }
+  }
+
+  private static int size(Node root) {
+    int size = 0;
+
+    for (Node child : root.children) {
+      int childSize = size(child);
+      size = size + childSize;
+    }
+    size = size + 1;
+
+    return size;
+  }
+
+  private static int max(Node root) {
+    int max = Integer.MIN_VALUE;
+    for (Node child : root.children) {
+      int childMax = max(child);
+      max = Math.max(max, childMax);
+    }
+    max = Math.max(max, root.data);
+    return max;
   }
 
   private static class Node {
